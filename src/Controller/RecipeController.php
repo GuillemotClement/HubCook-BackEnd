0aa889d06e74e4fde67f68b34205d8eb498add5b
GuillemotClement <guillemot.clement@protonmail.com>
2024-12-23 18:33:16 +0100

    Add method listRecip

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class RecipeController extends AbstractController
 {
@@ -17,9 +19,20 @@ class RecipeController extends AbstractController
         ]);
     }
 
-    #[Route('/recipes', name: 'listRecip', methods:['GET'])]
-    public function listRecipe(): JsonResponse
+    #[Route('/api/recipes', name: 'listRecipe', methods:['GET'])]
+    public function listRecipe(RecipeRepository $recipeRepository, SerializerInterface $serializer): JsonResponse
     {
+      //récupération des données en BDD
+      $recipeList = $recipeRepository->findAll();
 
+      //on serialize les datas récupérés
+      $jsonRecipeList = $serializer->serialize($recipeList, 'json');
+
+      //on retourne les données serializé
+      //1 : donnée serialisé
+      //2: code retour
+      //headers
+      //true: indique que les données sont serialisé
+      return new JsonResponse($jsonRecipeList, Response::HTTP_OK, [], true);
     }
 }

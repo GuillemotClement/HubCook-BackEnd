2a7d3c418f6b192326244015b7967ccfd0271c46
GuillemotClement <guillemot.clement@protonmail.com>
2024-12-25 22:02:14 +0100

    Add delete recipe

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Recipe;
 use App\Repository\RecipeRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use PhpParser\Builder\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,43 +14,61 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recipe', name: 'app_recipe')]
+    #[Route("/recipe", name: "app_recipe")]
     public function index(): Response
     {
-        return $this->render('recipe/index.html.twig', [
-            'controller_name' => 'RecipeController',
+        return $this->render("recipe/index.html.twig", [
+            "controller_name" => "RecipeController",
         ]);
     }
 
-    #[Route('/api/recipes', name: 'listRecipe', methods:['GET'])]
-    public function listRecipe(RecipeRepository $recipeRepository, SerializerInterface $serializer): JsonResponse
-    {
-      //récupération des données en BDD
-      $recipeList = $recipeRepository->findAll();
+    #[Route("/api/recipes", name: "listRecipe", methods: ["GET"])]
+    public function listRecipe(
+        RecipeRepository $recipeRepository,
+        SerializerInterface $serializer
+    ): JsonResponse {
+        //récupération des données en BDD
+        $recipeList = $recipeRepository->findAll();
 
-      //on serialize les datas récupérés
-      $jsonRecipeList = $serializer->serialize($recipeList, 'json');
+        //on serialize les datas récupérés
+        $jsonRecipeList = $serializer->serialize($recipeList, "json");
 
-      //on retourne les données serializé
-      //1 : donnée serialisé
-      //2: code retour
-      //headers
-      //true: indique que les données sont serialisé
-      return new JsonResponse($jsonRecipeList, Response::HTTP_OK, [], true);
+        //on retourne les données serializé
+        //1 : donnée serialisé
+        //2: code retour
+        //headers
+        //true: indique que les données sont serialisé
+        return new JsonResponse($jsonRecipeList, Response::HTTP_OK, [], true);
     }
 
-    #[Route('/api/recipes/{id}', name:'listDetail', methods:['GET'])]
-    public function listDetail(EntityManagerInterface $em, int $id, SerializerInterface $serializer): JsonResponse
-    {
-      //récupération de la recipe
-      $recipe = $em->getRepository(Recipe::class)->find($id);
+    #[Route("/api/recipes/{id}", name: "detailRecipe", methods: ["GET"])]
+    public function detailRecipe(
+        EntityManagerInterface $em,
+        int $id,
+        SerializerInterface $serializer
+    ): JsonResponse {
+        //récupération de la recipe
+        $recipe = $em->getRepository(Recipe::class)->find($id);
 
-      //si on as une recette, alors on la retourne
-      if($recipe){
-        $jsonRecipe = $serializer->serialize($recipe, 'json');
-        return new JsonResponse($jsonRecipe, Response::HTTP_OK, [], true);
-      }
+        //si on as une recette, alors on la retourne
+        if ($recipe) {
+            $jsonRecipe = $serializer->serialize($recipe, "json");
+            return new JsonResponse($jsonRecipe, Response::HTTP_OK, [], true);
+        }
 
-      return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+        return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+    }
+
+    #[Route("api/recipes/{id}", name: "deleteRecipe", methods: ["DELETE"])]
+    public function deleteRecipe(
+        Recipe $recipe,
+        EntityManagerInterface $em
+    ): JsonResponse {
+        //on passe la référence de la recipe
+        $em->remove($recipe);
+        //on exécute la requête
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
